ccadc280ac10d4a76a88e3d1de78658c159c3795
Patrick Rathje <pr@braune-digital.com>
2016-12-17 18:47:48 +0100

    Added IsNull and NotFilter

---
+++ Query/Filter/IsNullFilter.php
@@ -0,0 +1,34 @@
+<?php
+namespace BrauneDigital\QueryFilterBundle\Query\Filter;
+
+use BrauneDigital\QueryFilterBundle\Exception\InvalidConfigException;
+use BrauneDigital\QueryFilterBundle\Query\QueryBuilderJoinWrapperInterface;
+use BrauneDigital\QueryFilterBundle\Service\QueryManager;
+
+class IsNullFilter extends BaseFilter
+{
+    /**
+     * @param QueryBuilderJoinWrapperInterface $qbWrapper
+     * @param QueryManager $manager
+     * @param $alias
+     * @param $property
+     * @param $data
+     * @return mixed
+     */
+    public function getExpr(QueryBuilderJoinWrapperInterface $qbWrapper, QueryManager $manager, $alias, $property, $data, $optional = false)
+    {
+        $this->checkData($alias, $property, $data);
+        $path = $alias . "." . $property;
+        return $qbWrapper->getQueryBuilder()->expr()->isNull($path);
+    }
+
+    /**
+     * @param $alias
+     * @param $property
+     * @param $data
+     */
+    public function checkData($alias, $property, $data)
+    {
+        parent::checkData($alias, $property, $data);
+    }
+}
