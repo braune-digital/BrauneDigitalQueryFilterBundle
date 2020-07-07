ef5300169b3824c0c8f8548452a7c56f1cb726ee
Felix Baltruschat <fb@braune-digital.com>
2020-07-07 17:35:35 +0200

    fix new treebuild constructor

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,8 +18,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('bd_query_filter');
+        $treeBuilder = new TreeBuilder('bd_query_filter');
+        $rootNode = $treeBuilder->getRootNode();
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
